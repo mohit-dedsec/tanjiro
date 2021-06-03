f9b4f3b2c84e31b13623fa6f2c242d43d7428961
DedSec <mohit22sharma264@gmail.com>
2021-06-03 15:08:15 +0530

    Substract implementation with error

--- math.py
+++ math.py
@@ -1,12 +1,19 @@
 #addition function:-
 def add(x,y):
     return x+y
+
+
 #subtraction function:-
 def subtract(x,y):
-    return x-y
+	if y>x:
+    	return error
+    else:
+    	return x-y
+
 #Multiplication function:-
 def multiply(x,y):
     pass
+
 #Divide function:-
 def divide(x,y):
     pass
