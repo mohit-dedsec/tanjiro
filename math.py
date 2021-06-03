ed016bfcaed7b01d3289248d80c099597906dc29
DedSec <mohit22sharma264@gmail.com>
2021-06-03 14:47:59 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#addition function:-
 def add(x,y):
     pass
+#subtraction function:-
 def subtract(x,y):
     pass
+#Multiplication function:-
 def multiply(x,y):
     pass
+#Divide function:-
 def divide(x,y):
     pass
